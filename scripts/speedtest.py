17b2b3f69f9d0780ab505dea39a77d0ad5f45137
tuxudo <tuxudo@users.noreply.github.com>
2026-05-19 12:50:04 -0400

    Update speedtest.py

--- scripts/speedtest.py
+++ scripts/speedtest.py
@@ -270,7 +270,7 @@ class XmlDictConfig(dict):
         if list(parent_element.items()):
             self.update(dict(list(parent_element.items())))
         for element in parent_element:
-            if element:
+            if len(element):
                 # treat like dict - we assume that if the first two tags
                 # in a series are different, then they are all different.
                 if len(element) == 1 or element[0].tag != element[1].tag:
@@ -506,13 +506,16 @@ def main():
 
                     # Clean up bad cached entries
                     for idx, prev_result in enumerate(speedtest_cache):
-                        if prev_result['dl_throughput'].isdigit() and int(prev_result['dl_throughput']) > 0:
+                        if 'dl_throughput' in prev_result and prev_result['dl_throughput'].isdigit() and int(prev_result['dl_throughput']) > 0:
                             # debug_msg("good entry")
                             # debug_msg(prev_result['dl_throughput'])
                             pass
                         else:
-                            debug_msg("Bad dl_throughput entry: ")
-                            debug_msg(prev_result['dl_throughput'])
+                            if 'dl_throughput' not in prev_result:
+                                debug_msg("No dl_throughput entry!")
+                            else:
+                                debug_msg("Bad dl_throughput entry: ")
+                                debug_msg(prev_result['dl_throughput'])
                             del speedtest_cache[idx]
 
                     result = speedtest_cache
